e370568ae06e2036fed11348d39d943e016b5426
c.lopez <c.lopez@kmels.net>
2012-11-20 15:13:35 +0100

    attempt to sublist all descendents of ~/
    Ignore-this: 79b2666686861eb2dde9f5a01b5a5bd8
    
    darcs-hash:20121120141335-c3db2-8bbbf5e8e27df35f9267e0a62b6a21da7aae8a71

--- Main.hs
+++ Main.hs
@@ -2,16 +2,21 @@ import System.Directory
 import System.IO
 
 import System.INotify
+import System.Directory(getDirectoryContents)
+import Control.Monad(when)
 
 main :: IO ()
 main = do
     inotify <- initINotify
     print inotify
-    home <- getHomeDirectory    
+    home <- getHomeDirectory
+    indices <- subfiles home
+    putStrLn $ "Finished! " ++ (show $ indices)
+--    mapM_ $ putStrLn indices
     wd <- addWatch
             inotify
             [AllEvents]
-            (home ++ "/Downloads")
+            home
             --updateIndex 
             print
     
@@ -24,3 +29,21 @@ updateIndex :: Event -> IO ()
 updateIndex (Accessed isDirectory mfilePath) = putStrLn "Update access date"
 updateIndex (Created isDirectory mfilePath) = putStrLn "Create file"
 updateIndex e  = putStrLn $ "Unhandled event " ++ (show e)
+
+subfiles :: FilePath -> IO [FilePath]
+subfiles fp = do  
+  putStrLn $ "****************************************" ++ "Computing subfiles " ++ fp
+  isDir <- doesDirectoryExist fp  
+  if isDir
+  then 
+      case fp of
+        ".." -> return $ []
+        "." -> return $ []
+        _ -> do 
+          children <- getDirectoryContents fp
+          putStrLn $ "Computing children of " ++ (show children)
+          allDescendants <- mapM subfiles children
+          return $ concat allDescendants
+  else return [fp]
+  
+  
