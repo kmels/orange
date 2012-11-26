93bb64ba5f1c6de50ed3f197a201e234d5a9ae99
c.lopez <c.lopez@kmels.net>
2012-11-27 00:17:48 +0100

    Adds missing modules: Filesystem and Indexer
    Ignore-this: f91e32c50909bdc457e55dc436d0d14f
    
    darcs-hash:20121126231748-c3db2-0435b45dd4b5adb435038491d6a4fc8ee9f4afc3

---
+++ System/Orange/Indexer.hs
@@ -0,0 +1,50 @@
+module System.Orange.Indexer where
+
+import           Data.List             (inits)
+import           Data.List.Split       (splitOn)
+import           Database.Redis
+import           System.Posix.Files    (accessTime, fileID, getFileStatus,
+                                        isDirectory, modificationTime)
+
+import           Data.ByteString       (ByteString)
+import qualified Data.ByteString.Char8 as BS
+
+import           Foreign.C.Types       (CTime (..))
+import           System.Posix.Types    (EpochTime)
+import           Unsafe.Coerce
+
+import           Control.Exception     (IOException (..), try)
+
+-- | Indexes something about the file in a redis database
+indexFilePath :: FilePath -> Connection -> IO ()
+indexFilePath filePath redisConn = do
+  putStr $ "indexing " ++ filePath
+  fsEither <- try $ getFileStatus filePath  --get file status
+  case fsEither of
+    Left e -> do
+      putStrLn $ "... error, not indexed: " ++ show (e :: IOException)
+    Right fs -> do
+      let
+        tokens = concatMap (drop 1 . inits) $ splitOn "/" filePath
+        fid = fileID fs
+        fat = accessTime fs
+        fmt = modificationTime fs
+
+      _ <- runRedis redisConn $ do
+        set ("accessTime" <:> show fid) (toByteString fat)
+        set ("modificationTime" <:> show fid) (toByteString fmt)
+        set ("filePath" <:> show fid) (BS.pack filePath)
+
+      mapM_ (\token -> runRedis redisConn $ do
+                -- sort by modification date
+                zadd ("search" <:> token) ([(realToFrac fmt, toByteString fid)])
+            --we've just accessed it so all have the same access time
+            ) tokens
+      putStrLn " ... done "
+
+-- | Intercalates a colon. Used for redis key names.
+(<:>) :: String -> String -> ByteString
+(<:>) s s2 = BS.pack $ s ++ ":" ++ s2
+
+toByteString :: (Show a) => a -> ByteString
+toByteString = BS.pack . show
---
+++ System/Orange/Search.hs
@@ -0,0 +1,29 @@
+module System.Orange.Search where
+
+import           Control.Applicative   ((<|>))
+import           Data.Either           (rights)
+import           Data.Maybe            (catMaybes, fromJust)
+import           System.Orange.Indexer ((<:>))
+
+import qualified Data.ByteString.Char8 as BS
+import           Database.Redis
+
+-- | Searches for filepaths given a query
+searchFilePaths :: String -> Connection -> Int -> Int -> IO [BS.ByteString]
+searchFilePaths query redisConn from resultsCount = do
+  fids <- runRedis redisConn $ do
+    zrange ("search" <:> query) (fromIntegral from) (fromIntegral resultsCount) -- :: Either Reply [BS.ByteString]
+  case fids of
+    (Right fids') -> do
+      filePathMaybes <- mapM (\fid -> runRedis redisConn $ do
+                                 get ("filePath" <:> BS.unpack fid)
+                             ) fids'
+      return $ catMaybes . rights $ filePathMaybes
+    _ -> return []
+
+searchPaginated :: String -> Int -> Int -> IO [String]
+searchPaginated query s c = do
+  conn <- connect defaultConnectInfo
+  results <- searchFilePaths query conn s c
+  return $ map BS.unpack results
+
